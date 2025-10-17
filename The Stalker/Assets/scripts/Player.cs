0e3c2f15ac4ffc7c3a034d72797a8df2f13c239f
Weida Chen <weidac1@uci.edu>
2025-10-17 10:34:57 -0700

    basic prototype movement

--- The Stalker/Assets/scripts/Player.cs
+++ The Stalker/Assets/scripts/Player.cs
@@ -1,16 +1,48 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] InputActionAsset inputActions;
+    [SerializeField] float charSpeed;
+    [SerializeField] Rigidbody2D rb;
+    private Vector2 currentMoveInput;
+
+    private InputActionMap actionMap;
+    private InputAction moveAction;
+
+    void Awake()
     {
-        
+        actionMap = inputActions.FindActionMap("Player");
+        moveAction = actionMap.FindAction("Move");
+
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        moveAction.performed += OnMove;
+        moveAction.canceled += OnMove;
+
+        moveAction.Enable();
+    }
+    private void OnDisable()
+    {
+        moveAction.performed -= OnMove;
+        moveAction.canceled -= OnMove;
+
+        moveAction.Disable();
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(currentMoveInput.x * charSpeed, currentMoveInput.y * charSpeed);
+    }
+    
+    void OnMove(InputAction.CallbackContext ctx)
+    {
+        currentMoveInput = ctx.ReadValue<Vector2>();
     }
 }
