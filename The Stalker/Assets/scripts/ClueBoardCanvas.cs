2e5c56efad71a53f1be9effa19ef2073a651fcaf
bar-rod <brodasma@uci.edu>
2025-10-26 18:34:41 -0700

    clue board opens with e

--- The Stalker/Assets/scripts/ClueBoardCanvas.cs
+++ The Stalker/Assets/scripts/ClueBoardCanvas.cs
@@ -4,28 +4,63 @@ using UnityEngine.InputSystem;
 public class ClueBoardCanvas : MonoBehaviour
 {
     [SerializeField] private GameObject _ClueBoardCanvas;
-    [SerializeField] InputActionAsset inputActions;
+    [SerializeField] InputActionAsset playerControls;
 
+    private InputActionMap actionMap;
+    private InputAction playerInteract;
+    private bool _playerTouchingClue;
+    private bool ClueBoardOpen;
+    
     private void Awake()
     {
         _ClueBoardCanvas.SetActive(false);
+        actionMap = playerControls.FindActionMap("Player");
+        playerInteract = actionMap.FindAction("Interact");
+        _playerTouchingClue = false;
+        ClueBoardOpen = false;
+
     }
+    private void OnEnable()
+    {
+
+        //playerInteract = inputActions.Player.Interact;
+        
+        playerInteract.started += Interact;
+        playerInteract.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInteract.Disable();
+    }
+
+    private void Interact(InputAction.CallbackContext context)
+    {
+        if (_playerTouchingClue == true && ClueBoardOpen == false)
+        {
+            _ClueBoardCanvas.SetActive(true);
+            ClueBoardOpen = true;
+            //Debug.Log("We interacted with E key!");
+        }
+        else if (ClueBoardOpen == true)
+        {
+            _ClueBoardCanvas.SetActive(false);
+            ClueBoardOpen = false;
+        }
+        
+    }
+
+    
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        _ClueBoardCanvas.SetActive(true);
-        //Debug.Log("Clueboard is being touched");
-        /*
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            
-        }
-        */
+        _playerTouchingClue = true;
 
     }
     private void OnTriggerExit2D(Collider2D collider)
     {
-        _ClueBoardCanvas.SetActive(false);
+       // _ClueBoardCanvas.SetActive(false);
+        _playerTouchingClue = false;
     }
 
     void Start()
@@ -36,6 +71,11 @@ public class ClueBoardCanvas : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        /*
+        if (playerInteract.triggered)
+        {
+            Debug.Log("hi");
+        }
+        */
     }
 }
