528d9867e3fb4b7461047eaaa572607d6cf6ef90
Daphney Tsai <73204343+daphneyt214@users.noreply.github.com>
2025-10-26 20:07:59 -0700

    Timer
    implement basic countdown timer

---
+++ The Stalker/Assets/scripts/Timer.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour{
+    public float time = 300f;
+    public bool runnin = false;
+
+    private void Start()
+    {
+        runnin = true;
+    }
+
+    void Update()
+    {
+        if (runnin)
+        {
+            if (time > 0)
+            {
+                time -= Time.deltaTime;
+            }
+            else
+            {
+                Debug.Log("time ran out");
+                time = 0;
+                runnin = false;
+            }
+        }
+    }
+}
