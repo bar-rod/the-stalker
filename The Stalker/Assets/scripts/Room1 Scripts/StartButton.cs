b8ab70b13b282947cfb35e998d0ff16c5b0e09bd
adubey230 <dubeya2@uci.edu>
2025-10-31 16:22:12 -0700

    start button

---
+++ The Stalker/Assets/scripts/Room1 Scripts/StartButton.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartButton : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private Button button;
+    
+    void Start()
+    {
+        button.onClick.AddListener(OpenScene);
+    }
+    public void OpenScene()
+    {
+        Debug.Log("Reached");
+        SceneManager.LoadScene(2); 
+    }
+}
