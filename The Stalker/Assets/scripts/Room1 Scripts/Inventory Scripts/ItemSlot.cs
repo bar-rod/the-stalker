d7770ff6c9d336e8fa71923a98e2f59ffe082792
KrazyJax <165222334+KrazyJax@users.noreply.github.com>
2025-11-01 02:10:33 -0700

    added tooltips when you hover over an item in the inventory!

--- The Stalker/Assets/scripts/Room1 Scripts/Inventory Scripts/ItemSlot.cs
+++ The Stalker/Assets/scripts/Room1 Scripts/Inventory Scripts/ItemSlot.cs
@@ -1,16 +1,18 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class ItemSlot : MonoBehaviour
+public class ItemSlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] private int SlotID; // slot 1 has slotid 0, same as indexes in an array
-    [SerializeField] private Sprite emptySlotSprite;
-    const string emptySlotText = "Empty";
+    [SerializeField] private Sprite emptySlotSprite; // empty inventory slot icon needed
+    private bool hasItem;
 
+    const string emptySlotText = "Empty";
 
     private Inventory inventory;
     private Item item;
-    // Update is called once per frame
 
     private void Awake()
     {
@@ -20,22 +22,47 @@ public class ItemSlot : MonoBehaviour
     {
         if (inventory == null || inventory.inventoryList.Count == 0 || SlotID < 0 || SlotID >= inventory.inventoryList.Count)
         {
-            GetComponentInChildren<Text>().text = emptySlotText;
-            GetComponent<Image>().sprite = emptySlotSprite; // empty inventory slot icon needed
+            GetComponentInChildren<TMP_Text>().text = emptySlotText;
+            GetComponent<Image>().sprite = emptySlotSprite;
+            hasItem = false;
         }
         else
         {
             item = inventory.GetItemFromInventory(SlotID);
             if (item != null)
             {
-                GetComponentInChildren<Text>().text = item.itemName;
+                GetComponentInChildren<TMP_Text>().text = item.itemName;
                 GetComponent<Image>().sprite = item.itemSprite;
+                hasItem = true;
             }
             else
             {
-                GetComponentInChildren<Text>().text = emptySlotText;
+                GetComponentInChildren<TMP_Text>().text = emptySlotText;
                 GetComponent<Image>().sprite = emptySlotSprite;
+                hasItem = false;
             }
         }
+    }
+    //private void OnMouseEnter()
+    //{
+    //    if (hasItem)
+    //        TooltipManager._instance.SetAndShowTooltip(item.description);
+    //}
+
+    //private void OnMouseExit()
+    //{
+    //    TooltipManager._instance.HideTooltip();
+    //}
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (hasItem)
+            TooltipManager._instance.SetAndShowTooltip(item.description);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        TooltipManager._instance.HideTooltip();
     }
 }
