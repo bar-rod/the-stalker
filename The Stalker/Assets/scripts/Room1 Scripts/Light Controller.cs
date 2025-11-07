e5397f33128ed2bf1a3b21366a80e44fa35aba5d
Weida Chen <weidac1@uci.edu>
2025-11-06 22:22:13 -0800

    Update Light Controller's interaction codes to be consistent with the current interaction system

--- The Stalker/Assets/scripts/Room1 Scripts/Light Controller.cs
+++ The Stalker/Assets/scripts/Room1 Scripts/Light Controller.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Rendering.Universal;
-public class LightController : MonoBehaviour
+public class LightController : MonoBehaviour, Iinteractable
 {
     public float max_time;
     public float time;
@@ -19,29 +19,10 @@ public class LightController : MonoBehaviour
     private float game_over_timer = 20f;
     private bool player_turn_off;
 
-    private void Awake()
-    {
-        actionMap = playerControls.FindActionMap("Player");
-        playerInteract = actionMap.FindAction("Interact");
-        _playerTouchingLightSwitch = false;
-    }
-
-    private void OnEnable()
-    {
-        
-        playerInteract.started += Interact;
-        playerInteract.Enable();
-    }
-
-    private void OnDisable()
-    {
-       playerInteract.started -= Interact;
-        playerInteract.Disable();
-    }
     void Start()
     {
         max_time = timer.time;
-        
+
         //timer = GetComponentInParent<Timer>();
     }
 
@@ -50,17 +31,17 @@ public class LightController : MonoBehaviour
     {
         time = timer.time;
         // checks if light is on and cooldown is done to have chance for light to turn off
-        if ( isOf == false&& cooldown <=0) 
+        if (isOf == false && cooldown <= 0)
         {
-            randomFlicker(time,max_time);
+            randomFlicker(time, max_time);
         }
         // checks if light is off to and gambles for chance of light to turn on
-        else if (isOf == true&&time > max_time * 0.40 )
+        else if (isOf == true && time > max_time * 0.40)
         {
-            randomTurnOn(time,max_time);
+            randomTurnOn(time, max_time);
         }
         // if the current time is in between 20% of max time and 40% of max time then the light turns off at a set time between 15 to 30 seconds
-        else if (time > max_time * 0.2 && time < max_time * 0.40&& isOf == true && current_flicker_time <= 0f)
+        else if (time > max_time * 0.2 && time < max_time * 0.40 && isOf == true && current_flicker_time <= 0f)
         {
             turnLightsOn();
         }
@@ -70,28 +51,31 @@ public class LightController : MonoBehaviour
             current_flicker_time -= Time.deltaTime;
         }
         // decreases game over timer 
-        if(isOf == true){
-            game_over_timer-=Time.deltaTime;
+        if (isOf == true)
+        {
+            game_over_timer -= Time.deltaTime;
         }
         //if player has been in the dark for more than 20 seconds than its game over
-        if(game_over_timer <=0 ){
+        if (game_over_timer <= 0)
+        {
             gameObject.GetComponentInParent<GameOver>().ActivateGameOver();
-            
+
         }
         cooldown -= Time.deltaTime;
     }
     //turns light on and off
-    private void Interact(InputAction.CallbackContext context)
+    public void Interact(Collider2D collider)
     {
-        if (_playerTouchingLightSwitch == true && isOf == true)
+        if (isOf == true)
         {
             Debug.Log("Light Switch turned on");
-            if (!player_turn_off){
-            cooldown = 15f;
+            if (!player_turn_off)
+            {
+                cooldown = 15f;
             }
             turnLightsOn();
         }
-        else if (_playerTouchingLightSwitch == true && isOf == false)
+        else if (isOf == false)
         {
             Debug.Log("Light Switch turned off");
             player_turn_off = true;
@@ -103,15 +87,18 @@ public class LightController : MonoBehaviour
     // determines if light will turn off by getting a random int between 0 and the current time and checking if the random number 
     // is less than .1% of the max time. 
     // The chance of the light turing off will increase as time decreases.
-    public void randomFlicker(float time,float max_time)
+    public void randomFlicker(float time, float max_time)
     {
-        float threshold = max_time * 0.003f; 
+        float threshold = max_time * 0.003f;
         float randInt;
-        if(time < 0.2 * max_time){
+        if (time < 0.2 * max_time)
+        {
             threshold = 0.5f;
             randInt = Random.Range(0f, 100f);
-        }else{
-         randInt = Random.Range(0f, time);
+        }
+        else
+        {
+            randInt = Random.Range(0f, time);
         }
         if (randInt < threshold)
         {
@@ -123,13 +110,13 @@ public class LightController : MonoBehaviour
     }
 
     // does the opposite of the previous function. Determines random time for light to turn back on again
-    public void randomTurnOn(float time,float max_time)
+    public void randomTurnOn(float time, float max_time)
     {
         float threshold = max_time * 0.35f;
         float randInt = Random.Range(0f, time);
         if (randInt > threshold)
         {
-           turnLightsOn();
+            turnLightsOn();
 
         }
 
@@ -147,19 +134,15 @@ public class LightController : MonoBehaviour
     {
         SpotLight2D.intensity = 0;
         GlobaLight2D.intensity = 0.07f;
-        current_flicker_time = Random.Range(15f,30f);
+        current_flicker_time = Random.Range(15f, 30f);
         //flicker_time += 0.05f;
 
         isOf = true;
     }
-    private void OnTriggerEnter2D(Collider2D collider)
-    {
-        _playerTouchingLightSwitch = true;
 
-    }
-    private void OnTriggerExit2D(Collider2D collider)
+    public void CloseUI(Collider2D collider)
     {
-       
-        _playerTouchingLightSwitch = false;
+        //Do nothing
+        return;
     }
 }
