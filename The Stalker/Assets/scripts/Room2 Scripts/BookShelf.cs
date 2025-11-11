f0fa86d420654a65889e03a24dbe547ebc8f84d8
Michael Schneider <michaews@uci.edu>
2025-11-10 23:56:14 -0800

    Added Bookshelf Object in Final Room 2

---
+++ The Stalker/Assets/scripts/Room2 Scripts/BookShelf.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+
+public class Book : MonoBehaviour
+{     
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public class BookShelf : MonoBehaviour
+{
+    // 2D array.
+    Array shelves = Array.CreateInstance(typeof(Array), 3);
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        int bookCount = get
+        Array allBooks = Array.CreateInstance(typeof(Book), 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
