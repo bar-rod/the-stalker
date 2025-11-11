3be68ac0e51d492861de189ebb51be2f94efb3eb
bar-rod <brodasma@uci.edu>
2025-11-10 17:42:11 -0800

    PLEASE

--- The Stalker/Assets/scripts/Room1 Scripts/ClueBoard.cs
+++ The Stalker/Assets/scripts/Room1 Scripts/ClueBoard.cs
@@ -26,7 +26,7 @@ IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
         //Debug.Log("OnBeginDrag");
     }
 
-    //:P
+    //PLEASE 
 
     public void OnEndDrag(PointerEventData eventData)
     {
