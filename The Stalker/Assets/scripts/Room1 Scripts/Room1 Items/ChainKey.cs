9481e4cefc935d2ce28e4131f946e10e839f363a
KrazyJax <165222334+KrazyJax@users.noreply.github.com>
2025-11-20 12:51:36 -0800

    final touches on inventory, updated gameover screen, added functionality to chain key

--- The Stalker/Assets/scripts/Room1 Scripts/Room1 Items/ChainKey.cs
+++ The Stalker/Assets/scripts/Room1 Scripts/Room1 Items/ChainKey.cs
@@ -2,15 +2,15 @@ using UnityEngine;
 
 public class ChainKey : Item
 {
+
+    public ChainConstraint chainConstraint;
+    public TargetJoint2D chainConnector;
     public override bool UseItem() 
     {
         Debug.Log("using chain key");
 
-        // message pop up: "Press 'i' or 'tab' to open your inventory"
-        // wait for player to open inventory
-        // message pop up: "Click an item to use it"
-        // player clicks on chain key
-        // disable the chainconstraint script, detach the target joint 2d
+        chainConstraint.BreakChain();
+        chainConnector.enabled = false;
 
         return true; // item is consumed on use
     }
