2e1f8f8e4724cc487cfe74ff04f5b1ca759c6bfa
Michael Schneider <michaews@uci.edu>
2026-02-01 22:11:40 -0800

    Implemented tire destroy on click for shelf seq

---
+++ The Stalker/Assets/scripts/Room3 Scripts/Shelf Scripts/collectTires.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.UI; 
+
+
+
+public class CollectTires : MonoBehaviour
+{
+
+    public void OnButtonClick()
+    {
+        Destroy(this.gameObject);
+
+        // add code for collecting the tire in the inventory here
+    }
+}
