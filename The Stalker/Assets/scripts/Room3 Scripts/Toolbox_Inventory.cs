8a55f10225deae1a0f1ff0c8f5e191dafa45b769
Michael Schneider <michaews@uci.edu>
2026-02-12 17:29:27 -0800

    Converting toolbox to use key (WIP)

---
+++ The Stalker/Assets/scripts/Room3 Scripts/Toolbox_Inventory.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+
+public class Vent_Inventory : PuzzleInteractable
+{
+     [SerializeField] private GameObject theCanvas;
+     [SerializeField] public bool in_vent; 
+     private bool bCanvasActive = false;
+    public override void UseItem(Item item)
+    {
+         if (itemIDNeeded == item.id)
+            {
+                // this is where behavior would go for solving the puzzle
+                Debug.Log(item.name + (" is the correct item"));
+                //inventory.ToggleInventory();
+
+                theCanvas.SetActive(true);
+                isSolved = true;
+
+            }
+            else
+            {
+                // this is where you would have a hint message pop up to guide the player to the correct item (or a different fail condition)
+                Debug.Log(item.name + " is the incorrect item");
+            }
+        //base.CloseUI(null);
+    }
+    public override bool Interact()
+    {
+        Debug.Log("Called Interact() from PuzzleInteractable");
+        
+        if (isSolved&&bCanvasActive==false) {
+            theCanvas.SetActive(true);
+            bCanvasActive=true;
+            return true;
+        }
+        else if(in_vent==false&&bCanvasActive==false){
+
+            if (!LocatorDialogue2.Instance.Dialogue2Script.ElisaAudioPlaying)
+            {
+                LocatorDialogue2.Instance.Dialogue2Script.ShowElisaText("What can I use to open the vent?", 2);
+            }
+            
+
+            if(inventory.GetInventoryOpen()==false){
+            inventory.ToggleInventory();
+            }
+            in_vent=true;
+            bCanvasActive=true;
+            return true;
+        }
+        else if(bCanvasActive==true){
+            CloseUI();
+            bCanvasActive=false;
+            return false;
+        }
+
+        return false;
+        //inventory.OpenForPuzzle(UseItem);
+    }
+
+    public override void CloseUI()
+    {
+        if(theCanvas.activeSelf == true){
+            theCanvas.SetActive(false);
+            in_vent=false;
+        }
+        else
+        {
+             inventory.ToggleInventory();
+             in_vent=false;
+        }
+
+    }
+    // helper functions are ok, just make them private if possible
+}
+
