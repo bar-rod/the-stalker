2cd5fd04e412a02f10d1a78c439a62ca018304be
adubey230 <dubeya2@uci.edu>
2026-02-19 23:36:39 -0800

    colliders + assets

---
+++ The Stalker/Assets/scripts/Room3 Scripts/SpriteOrder.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class SpriteOrder : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private GameObject player;
+    [SerializeField] private SpriteRenderer _sprite;
+    [SerializeField] private GameObject car;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (car.transform.position.y < player.transform.position.y)
+        {
+            _sprite.sortingOrder = 3;
+        }
+        else
+        {
+            _sprite.sortingOrder = 7;
+        }
+    }
+}
