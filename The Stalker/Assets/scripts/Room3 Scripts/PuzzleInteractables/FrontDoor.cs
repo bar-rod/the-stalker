d0d6ed600fe016cafff287834e8b219c442eddc3
adubey230 <dubeya2@uci.edu>
2026-02-22 20:33:26 -0800

    front door interactions + key ui + key hover state

---
+++ The Stalker/Assets/scripts/Room3 Scripts/PuzzleInteractables/FrontDoor.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class FrontDoor : PuzzleInteractable
+{
+    [Header("UI")]
+    [SerializeField] private Canvas puzzleCanvas;
+
+    private bool isOpen = false;
+
+    protected override void Start()
+    {
+        base.Start();
+        puzzleCanvas.gameObject.SetActive(false);
+    }
+
+    public override bool Interact()
+    {
+        if (isSolved)
+            return false;
+
+        // If canvas closed → open it
+        if (!isOpen)
+        {
+            puzzleCanvas.gameObject.SetActive(true);
+            isOpen = true;
+        }
+        // If canvas open → close it
+        else
+        {
+            puzzleCanvas.gameObject.SetActive(false);
+            isOpen = false;
+            CloseUI();
+        }
+
+        return true;
+    }
+
+    public override void UseItem(Item item)
+    {
+        // We don't need this if items aren't "used"
+    }
+}
---
+++ The Stalker/Assets/scripts/Room3 Scripts/key.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class key : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private Image keyImage;
+    [SerializeField] private Sprite keyOg;
+    [SerializeField] private Sprite keyOutline;
+
+    void Update()
+    {
+
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        keyImage.sprite = keyOutline;
+        Debug.Log("Mouse is over key");
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        keyImage.sprite = keyOg;
+        Debug.Log("Mouse is not over key");
+    }
+    
+}
